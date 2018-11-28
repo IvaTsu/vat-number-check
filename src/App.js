1adc99ff4e7a0fc935d225ba5bfae04cb1681cea
IvaTsu <ivan.tsud@gmail.com>
2018-11-28 20:19:10 +0200

    Add axios and basic data fetching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      results: {}
+    };
+  }
+
+  _updateInputvalue = e =>
+    this.setState({
+      inputValue: e.target.value
+    });
+
+  _doRequest = () => {
+    axios
+      .get(`https://vat.erply.com/numbers?vatNumber=${this.state.inputValue}`)
+      .then(response => {
+        this.setState({ results: response.data });
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function() {
+        // always executed
+      });
+  };
+
   render() {
     return (
       <div className="App">
@@ -20,6 +49,28 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <label>
+          VAT number:
+          <input
+            type="text"
+            name="vat"
+            value={this.state.inputValue}
+            onChange={e => this._updateInputvalue(e)}
+          />
+        </label>
+        <button onClick={this._doRequest}>CLICK</button>
+
+        {this.state.results && (
+          <div>
+            <p>{this.state.results.CountryCode}</p>
+            <p>{this.state.results.VATNumber}</p>
+            <p>{this.state.results.RequestDate}</p>
+            <p>{this.state.results.Valid}</p>
+            <p>{this.state.results.Name}</p>
+            <p>{this.state.results.Address}</p>
+          </div>
+        )}
       </div>
     );
   }
