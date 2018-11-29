3629a9ee0d03265e31a229481749a679936cbb86
IvaTsu <ivan.tsud@gmail.com>
2018-11-29 02:38:58 +0200

    Import and implement components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Results from "./components/Results";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       inputValue: "",
-      results: {}
+      results: null,
+      loading: false,
+      error: false
     };
   }
 
@@ -17,60 +21,40 @@ class App extends Component {
       inputValue: e.target.value
     });
 
-  _doRequest = () => {
+  _getData = e => {
+    this.setState({ loading: true });
+    e.preventDefault();
     axios
       .get(`https://vat.erply.com/numbers?vatNumber=${this.state.inputValue}`)
       .then(response => {
-        this.setState({ results: response.data });
+        this.setState({ results: response.data, loading: false, error: false });
       })
-      .catch(function(error) {
+      .catch(error => {
         // handle error
         console.log(error);
-      })
-      .then(function() {
-        // always executed
+        this.setState({
+          error: true,
+          loading: false
+        });
       });
   };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
-        <label>
-          VAT number:
-          <input
-            type="text"
-            name="vat"
-            value={this.state.inputValue}
-            onChange={e => this._updateInputvalue(e)}
-          />
-        </label>
-        <button onClick={this._doRequest}>CLICK</button>
-
-        {this.state.results && (
-          <div>
-            <p>{this.state.results.CountryCode}</p>
-            <p>{this.state.results.VATNumber}</p>
-            <p>{this.state.results.RequestDate}</p>
-            <p>{this.state.results.Valid}</p>
-            <p>{this.state.results.Name}</p>
-            <p>{this.state.results.Address}</p>
-          </div>
-        )}
+      <div className="app-container">
+        <Header title="VAT Number Check" />
+        <Form
+          value={this.state.inputValue}
+          onChange={e => this._updateInputvalue(e)}
+          onClick={e => this._getData(e)}
+          disabled={!this.state.inputValue}
+          loading={this.state.loading}
+        />
+        <Results
+          error={this.state.error}
+          loading={this.state.loading}
+          results={this.state.results}
+        />
       </div>
     );
   }
